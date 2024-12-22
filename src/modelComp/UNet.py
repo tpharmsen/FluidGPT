f4fa585bbc08477ede5c0a6a62546455b3961c15
tpharmsen <t.p.harmsen@student.tue.nl>
2024-12-22 15:10:23 +0100

    bubbleML files

---
+++ src/modelComp/UNet.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class UNet2D(nn.Module):
+    def __init__(self, in_channels, out_channels, features=(64, 128, 256, 512)):
+        super(UNet2D, self).__init__()
+
+        self.encoder = nn.ModuleList([
+            self._conv_block(in_channels if i == 0 else features[i - 1], feature)
+            for i, feature in enumerate(features)
+        ])
+
+        self.bottleneck = self._conv_block(features[-1], features[-1] * 2)
+
+        self.decoder = nn.ModuleList([
+            nn.Sequential(
+                nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2),
+                self._conv_block(feature * 2, feature)
+            )
+            for feature in reversed(features)
+        ])
+
+        self.final_layer = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skip_connections = []
+        for down in self.encoder:
+            x = down(x)
+            skip_connections.append(x)
+            x = F.max_pool2d(x, kernel_size=2, stride=2)
+
+        x = self.bottleneck(x)
+        skip_connections.reverse()
+
+        for idx, up in enumerate(self.decoder):
+            x = up[0](x)
+            skip = skip_connections[idx]
+            #if x.shape != skip.shape:
+            #    x = F.interpolate(x, size=skip.shape[2:])
+            x = torch.cat((skip, x), dim=1)
+            x = up[1](x)
+
+        return self.final_layer(x)
+
+    @staticmethod
+    def _conv_block(in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.GELU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.GELU(),
+        )
