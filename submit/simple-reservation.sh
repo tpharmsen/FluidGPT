39b35d24da2554b6ab92700106e03e4cde928a80
Harmsen <t.p.harmsen@student.tue.nl>
2024-12-26 15:54:58 +0100

    base script for submission snellius

---
+++ submit/simple-reservation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=simplejob
+#SBATCH --partition=gpu_mig
+#SBATCH --time=30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+#SBATCH --reservation=jhs_tue2022
+
+module load 2022
+module load Anaconda3/2022.05
+. "/sw/arch/RHEL8/EB_production/2022/software/Anaconda3/2022.05/etc/profile.d/conda.sh"
+conda activate grad311
