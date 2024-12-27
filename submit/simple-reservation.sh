4d15500ba1e52b11a82e84c13f21b3f06d01023d
tpharmsen <t.p.harmsen@student.tue.nl>
2024-12-28 00:54:16 +0100

    more time

--- submit/simple-reservation.sh
+++ submit/simple-reservation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=loadmoduletest
 #SBATCH --partition=gpu_mig
-#SBATCH --time=40:00
+#SBATCH --time=01:20:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
