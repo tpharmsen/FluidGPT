d1729715c192bc7dfed746afd04cc9160733bd26
Harmsen <t.p.harmsen@student.tue.nl>
2025-01-02 17:24:26 +0100

    for short tests on hpc

---
+++ submit/simple-reservation-short.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=loadmoduletest
+#SBATCH --partition=gpu_mig
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+#SBATCH --reservation=jhs_tue2022
+
+module load 2022
+module load Anaconda3/2022.05
+. "/sw/arch/RHEL8/EB_production/2022/software/Anaconda3/2022.05/etc/profile.d/conda.sh"
+conda activate grad312
+cd AutoregressiveNeuralOperators
+
+python -c "import torch; print('cuda available: ', torch.cuda.is_available())"
+python src/PFtrainer.py --conf conf/example.yaml
