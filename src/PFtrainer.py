fcf85d768d04b605f9848af27eaa27d765de6fae
Harmsen <t.p.harmsen@student.tue.nl>
2025-01-02 18:27:50 +0100

    logging time as well, train_elem loss

--- src/PFtrainer.py
+++ src/PFtrainer.py
@@ -251,7 +251,7 @@ def main(args: argparse):
                 "val_loss_timestep": val_loss_timestep.item(),
                 "val_loss_unrolled": val_loss_unrolled.item(),
                 "len_train_losses": len(train_losses), 
-                "elapsed_time": time.time() - start_time(),
+                "elapsed_time": time.time() - start_time,
                 "timestamp": datetime.now().strftime("%H:%M:%S")
             #    "train_losses_elements": table_train_losses
             })
