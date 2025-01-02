7d5043b825581629899e77cb44836eaabb4d976b
Harmsen <t.p.harmsen@student.tue.nl>
2025-01-02 18:32:25 +0100

    logging time as well, train_elem loss

--- src/PFtrainer.py
+++ src/PFtrainer.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-import datetime
+from datetime import datetime
 import time
 import wandb
 import argparse
