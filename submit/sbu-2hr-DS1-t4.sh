823df866d83019540706f6c75681580f3d5a9d47
tpharmsen <t.p.harmsen@student.tue.nl>
2025-02-04 14:00:21 +0100

    check for cuda

---
+++ submit/sbu-2hr-DS1-t4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=t3a100
+#SBATCH --partition=gpu_a100
+#SBATCH --time=00:02:00
+#SBATCH --gres=gpu:1
+# #SBATCH --cpus-per-task=4
+
+# #SBATCH -N 1
+module load 2023
+module load Anaconda3/2023.07-2
+. "/sw/arch/RHEL8/EB_production/2023/software/Anaconda3/2023.07-2/etc/profile.d/conda.sh"
+conda activate grad311_hpc2023
+cd AutoregressiveNeuralOperators
+
+python -c "import torch; print('cuda available: ', torch.cuda.is_available())"
