5d99f407cd77b3cdd5166f93802fe5a2daf2e487
tpharmsen <t.p.harmsen@student.tue.nl>
2025-02-07 02:01:36 +0100

    reserv 10m subm

---
+++ submit/jhs-10m-1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=jhs2hr1
+#SBATCH --partition=gpu_mig
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+#SBATCH --reservation=jhs_tue2022
+
+module load 2023
+module load Anaconda3/2023.07-2
+. "/sw/arch/RHEL8/EB_production/2023/software/Anaconda3/2023.07-2/etc/profile.d/conda.sh"
+conda activate grad311_hpc2023
+cd AutoregressiveNeuralOperators
+
+python src/train.py --conf conf/hpc_01DS4_1.yaml --trainer PFTB
