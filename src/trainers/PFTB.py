67efad9c10fc2d795cdb0e4e627c64619fc42044
Harmsen <t.p.harmsen@student.tue.nl>
2025-02-24 13:52:36 +0100

    changed advanced lr scheduler to simple version

--- src/trainers/PFTB.py
+++ src/trainers/PFTB.py
@@ -129,12 +129,12 @@ class PFTBTrainer:
         #self.scheduler = StepLR(self.optimizer, step_size=20, gamma=0.1)
         #self.scheduler = ReduceLROnPlateau(self.optimizer, mode='min', factor=0.1, patience=20, min_lr=1e-6)
         #self.scheduler = CosineAnnealingLR(self.optimizer, T_max=70, eta_min=1e-6)
-        scheduler_milestone = 60
-        cosine_scheduler = CosineAnnealingLR(self.optimizer, T_max=scheduler_milestone, eta_min=1e-7)
-        constant_scheduler = ConstantLR(self.optimizer, factor=1e-6, total_iters=500)
+        #scheduler_milestone = 60
+        #cosine_scheduler = CosineAnnealingLR(self.optimizer, T_max=scheduler_milestone, eta_min=1e-7)
+        #constant_scheduler = ConstantLR(self.optimizer, factor=1e-6, total_iters=500)
 
-        self.scheduler = SequentialLR(self.optimizer, schedulers=[cosine_scheduler, constant_scheduler], milestones=[scheduler_milestone])
-
+        #self.scheduler = SequentialLR(self.optimizer, schedulers=[cosine_scheduler, constant_scheduler], milestones=[scheduler_milestone])
+        self.scheduler = StepLR(self.optimizer, step_size=20, gamma=0.1)
 
     def nparams(self, model):
         return sum(p.numel() for p in model.parameters() if p.requires_grad)
