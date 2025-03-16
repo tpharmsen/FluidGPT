f27b2afbdd655d0d0d4739bb5ba703c8645f25b4
tpharmsen <t.p.harmsen@student.tue.nl>
2025-03-16 23:29:58 +0100

    pde bench downloader

--- src/scripts/PDEBenchDownload.sh
+++ src/scripts/PDEBenchDownload.sh
@@ -1,19 +1,11 @@
 
-CSV_FILE="../../src/scripts/pdebench_data_urls.csv"
+#!/bin/bash
 
-cd data/prjs1359
-mkdir -p PDEBenchSet 
+CSV_FILE="src/scripts/pdebench_data_urls.csv"
+mkdir -p data/prjs1359/PDEBenchSet
 
-# print hellow world
+# Print Hello World
 echo "Hello, World!"
-
-# Read the CSV and download the files
-#tail -n +2 "$CSV_FILE" | while IFS=',' read -r _ filename url path _; do
-    # print the file url
-    #echo "$url"
-    #mkdir -p "$path"
-    #wget -c "$url" -O "downloads/$path/$filename"
-#done
 
 # Check if at least one index is provided
 if [ $# -eq 0 ]; then
@@ -24,7 +16,7 @@ fi
 # Read the CSV file and loop over the provided indices
 tail -n +2 "$CSV_FILE" | nl -v 0 -w 1 -s ',' | while IFS=',' read -r idx _ filename url path _; do
     for arg in "$@"; do
-        if [[ "$idx" -eq "$arg" ]]; then
+        if [ "$idx" -eq "$arg" ]; then
             echo "Downloading: $filename"
             mkdir -p "downloads/$path"
             wget -c "$url" -O "downloads/$path/$filename"
