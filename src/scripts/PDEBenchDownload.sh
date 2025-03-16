df1025c4b792b8c7b469bef4b07d4e459c3bbd7c
tpharmsen <t.p.harmsen@student.tue.nl>
2025-03-16 23:33:11 +0100

    pde bench downloader

--- src/scripts/PDEBenchDownload.sh
+++ src/scripts/PDEBenchDownload.sh
@@ -1,8 +1,10 @@
-
 #!/bin/bash
 
 CSV_FILE="src/scripts/pdebench_data_urls.csv"
-mkdir -p data/prjs1359/PDEBenchSet
+SAVE_DIR="data/prjs1359/NS_incom_inhom"
+
+# Create target directory
+mkdir -p "$SAVE_DIR"
 
 # Print Hello World
 echo "Hello, World!"
@@ -14,11 +16,11 @@ if [ $# -eq 0 ]; then
 fi
 
 # Read the CSV file and loop over the provided indices
-tail -n +2 "$CSV_FILE" | nl -v 0 -w 1 -s ',' | while IFS=',' read -r idx _ filename url path _; do
+tail -n +2 "$CSV_FILE" | nl -v 0 -w 1 -s ',' | while IFS=',' read -r idx _ filename url _ _; do
     for arg in "$@"; do
         if [ "$idx" -eq "$arg" ]; then
             echo "Downloading: $filename"
-            wget -c "$url" -O "data/prjs1359/$path/$filename"
+            wget -c "$url" -O "$SAVE_DIR/$filename"
         fi
     done
 done
