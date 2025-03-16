e6440f21b6deb6f0f033cee10f543e56ab9fc16b
Harmsen <t.p.harmsen@student.tue.nl>
2025-03-16 16:44:55 +0100

    download script for amira meshes

--- src/scripts/AmiraDownload.sh
+++ src/scripts/AmiraDownload.sh
@@ -1,5 +1,8 @@
 #wget https://libdrive.ethz.ch/index.php/s/lv7dV40oYlkWJiC/download?path=%2F&files=0000.am
 #wget https://libdrive.ethz.ch/index.php/s/lv7dV40oYlkWJiC/download?path=%2F"&"files=0000.am
+
+mkdir -p AmiraSet 
+
 for i in {0,10,100}; do
-    wget "https://libdrive.ethz.ch/index.php/s/lv7dV40oYlkWJiC/download?path=%2F&files=$(printf "%04d" $i).am"
+    wget -O "AmiraSet/$(printf "%04d" $i).am" "https://libdrive.ethz.ch/index.php/s/lv7dV40oYlkWJiC/download?path=%2F&files=$(printf "%04d" $i).am"
 done
