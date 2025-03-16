cc1d9ee3ea5b5498e0f38f881d9ccea6980c8732
tpharmsen <t.p.harmsen@student.tue.nl>
2025-03-16 23:13:28 +0100

    pde bench downloader

--- src/scripts/PDEBenchDownload.sh
+++ src/scripts/PDEBenchDownload.sh
@@ -4,10 +4,13 @@ CSV_FILE="pdebench_data_urls.csv"
 cd data/prjs1359
 mkdir -p PDEBenchSet 
 
+# print hellow world
+echo "Hello, World!"
+
 # Read the CSV and download the files
-tail -n +2 "$CSV_FILE" | while IFS=',' read -r _ filename url path _; do
+#tail -n +2 "$CSV_FILE" | while IFS=',' read -r _ filename url path _; do
     # print the file url
-    echo "$url"
+    #echo "$url"
     #mkdir -p "$path"
     #wget -c "$url" -O "downloads/$path/$filename"
-done
+#done
