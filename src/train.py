2883d0a7ea8a6dd5cd6ad389765238a8fa3c0bc5
Tom Harmsen <tharmsen@gcn4.local.snellius.surf.nl>
2025-03-31 20:03:40 +0200

    new conf struct and train.py

--- src/train.py
+++ src/train.py
@@ -1,11 +1,42 @@
 import argparse
+import yaml
 
+
+def load_yaml(file_path):
+    with open(file_path, "r") as f:
+        return yaml.safe_load(f)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Train a model on the BubbleML dataset')
-    parser.add_argument("--conf", type=str, default="conf/example.yaml")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--CB", type=str, default="conf/base/std.yaml")
+    parser.add_argument("--CD", type=str, default="conf/data/std.yaml")
+    parser.add_argument("--CM", type=str, default="conf/model/std.yaml")
+    parser.add_argument("--CT", type=str, default="conf/training/std.yaml")
+    parser.add_argument("--CV", type=str, default="conf/validation/std.yaml")
     parser.add_argument("--trainer", type=str, default="PFTB")
     args = parser.parse_args()
+
+    if os.path.exists(args.CB):
+        cb = load_yaml(args.CB)
+    else:
+        raise FileNotFoundError(f"Config file {args.CB} not found.")
+    if os.path.exists(args.CD):
+        cd = load_yaml(args.CD)
+    else:
+        raise FileNotFoundError(f"Config file {args.CD} not found.")
+    if os.path.exists(args.CM):
+        cm = load_yaml(args.CM)
+    else:
+        raise FileNotFoundError(f"Config file {args.CM} not found.")
+    if os.path.exists(args.CT):
+        ct = load_yaml(args.CT)
+    else:
+        raise FileNotFoundError(f"Config file {args.CT} not found.")
+    if os.path.exists(args.CV):
+        cv = load_yaml(args.CV)
+    else:
+        raise FileNotFoundError(f"Config file {args.CV} not found.")
+
     if args.trainer == "simple":
         from trainers.simple import SimpleTrainer
         trainer = SimpleTrainer(args.conf)
@@ -15,6 +46,10 @@ if __name__ == "__main__":
     elif args.trainer == "PFTB":
         from trainers.PFTB import PFTBTrainer
         trainer = PFTBTrainer(args.conf)
+    elif args.trainer == "STT":
+        from trainers.simpleTransformerTrainer import simpleTransformerTrainer
+        trainer = simpleTransformerTrainer(cb, cd, cm, ct, cv)
     else:
         raise ValueError("Trainer not set")
+
     trainer.train()
---
+++ src/trainers/simpleTransformerTrainer.py
@@ -0,0 +1,6 @@
+class simpleTransformerTrainer:
+    def __init__(self, cb, cd, cm, ct, cv):
+        print('test')
+        print(cb['data_path'])
+    def train(self):
+        print('tralala')
