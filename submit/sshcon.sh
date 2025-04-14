949e69028c3288d3e2341ef5ff5443b60a396600
Tom Harmsen <tharmsen@gcn2.local.snellius.surf.nl>
2025-04-14 17:34:13 +0200

    ssh connect script

---
+++ submit/sshcon.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=sshconnect
+#SBATCH --partition=gpu_mig
+#SBATCH --time=02:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-node=1
+#SBATCH --reservation=terv92681
+
+module load 2024
+module load Anaconda3/2024.06-1
+source "/sw/arch/RHEL9/EB_production/2024/software/Anaconda3/2024.06-1/etc/profile.d/conda.sh"
+conda activate grad312
+cd AutoregressiveNeuralOperators
+
