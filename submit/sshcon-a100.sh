9d46acaff3f23a900b3346d98a2a47024ac0595d
Harmsen <t.p.harmsen@student.tue.nl>
2025-04-15 16:01:16 +0200

    4hr reserv for ssh a100 and mig

--- submit/sshcon-a100.sh
+++ submit/sshcon-a100.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-node=1
 #BATCH --reservation=terv92681
-#SBATCH --wrap="sleep infinity"
+#SBATCH --wrap="sleep infinity"
 
 module load 2024
 module load Anaconda3/2024.06-1
