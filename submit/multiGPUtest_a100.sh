ef24bb714ec72f4dd0ffd8d3c53762235a05739f
Tom Harmsen <tharmsen@gcn4.local.snellius.surf.nl>
2025-04-16 23:02:40 +0200

    test on multi a100 and h100

--- submit/multiGPUtest_a100.sh
+++ submit/multiGPUtest_a100.sh
@@ -7,6 +7,7 @@
 #SBATCH --gpus-per-node=3
 
 module load 2024
+module spider CUDA/12.6.0
 module load Anaconda3/2024.06-1
 source "/sw/arch/RHEL9/EB_production/2024/software/Anaconda3/2024.06-1/etc/profile.d/conda.sh"
 conda activate grad312
