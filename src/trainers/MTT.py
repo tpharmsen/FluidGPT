2e2671b8da04009dec116be7fac95e801f217d45
Tom Harmsen <tharmsen@gcn4.local.snellius.surf.nl>
2025-04-16 22:22:29 +0200

    gpu_mig bug fix

--- src/trainers/MTT.py
+++ src/trainers/MTT.py
@@ -53,7 +53,7 @@ class MTT:
             devices= 'auto',
             logger=wandb_logger,#num_gpus,
             #log_every_n_steps=0,
-            callbacks=[LoggerCallback(self.cb, self.cd, self.cm, self.ct)],
+            #callbacks=[LoggerCallback(self.cb, self.cd, self.cm, self.ct)],
             #strategy="ddp"
             max_epochs=self.ct.epochs,
         )
@@ -95,10 +95,17 @@ class MTTmodel(pl.LightningModule):
         self.cm = cm
         self.ct = ct
 
+        self.base_path = self.cb.save_path + self.cb.folder_out
+        self.out_0 = self.base_path + self.ct.plottrain_out
+        self.out_1 = self.base_path + self.ct.plotval_out
+        self.out_2 = self.base_path + self.ct.plotvalf_out
+        self.out_3 = self.base_path + self.ct.anim_out
+
         self.train_losses = []
         self.val_SS_losses = []
         self.val_FS_losses = []
-        self.epoch_start_time = None
+        self.epoch_time = None
+        self.log_time = None
 
         self._initialize_model()   
         self.counter = 0
@@ -133,6 +140,7 @@ class MTTmodel(pl.LightningModule):
         #self.log("train_loss", train_loss, on_step=False, on_epoch=True, prog_bar=True)
         #return train_loss
         self.train_losses.append(train_loss.item())
+        return train_loss
 
     def validation_step(self, batch, batch_idx, dataloader_idx):
         # Validation logic
@@ -146,6 +154,7 @@ class MTTmodel(pl.LightningModule):
             self.val_FS_losses.append(val_loss.item())
             #self.log("val_FS_loss", val_loss, on_step=False, on_epoch=True, prog_bar=False)
         #return {'val_loss': val_loss, 'dataloader_idx': dataloader_idx}
+        return val_loss
 
 
     def test_step(self, batch, batch_idx):
@@ -154,7 +163,7 @@ class MTTmodel(pl.LightningModule):
         y_hat = self(x)
         test_loss = F.mse_loss(y_hat, y)
         #self.log("test_loss", test_loss, on_step=False, on_epoch=True, prog_bar=True)
-        #return test_loss
+        return test_loss
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(
@@ -179,32 +188,115 @@ class MTTmodel(pl.LightningModule):
         return {"optimizer": optimizer, "lr_scheduler": scheduler}
     
     def on_train_epoch_start(self):
-        self.epoch_time = time.time()
+        if not self.trainer.sanity_checking:
+            self.epoch_time = time.time()
 
     def on_validation_epoch_end(self):
         if not self.trainer.sanity_checking:
+            epoch = self.trainer.current_epoch
             self.epoch_time = time.time() - self.epoch_time
+            self.log_time = time.time()
 
             train_loss = np.mean(self.train_losses)
             val_SS_loss = np.mean(self.val_SS_losses)
             val_FS_loss = np.mean(self.val_FS_losses)
-            
-            self.logger.experiment.log({
-                "epoch": self.current_epoch,
-                "train_loss": train_loss,
-                "val_SS_loss": val_SS_loss,
-                "val_FS_loss": val_FS_loss,
-                "Learning Rate": self.trainer.optimizers[0].param_groups[0]['lr'],
-                "Epoch Time": self.epoch_time
-            })
 
             self.log_dict({
                 "val_SS_loss": val_SS_loss,
             }, prog_bar=False)
 
+            visuals = self.cb.viz and epoch % self.cb.viz_freq == 0
+            if visuals:
+                device = 'cuda' if torch.cuda.is_available() else 'cpu'
+                self.make_plot(self.out_1, mode='val', device=device)
+                self.make_plot(self.out_0, mode='train', device=device)
+                self.make_plot(self.out_2, mode='val_forward', device=device)
+                #self.make_anim(model, self.out_3, device=device)
+            
+            self.log_time = time.time() - self.log_time
+            self.logger.experiment.log({
+                "epoch": epoch,
+                "train_loss": train_loss,
+                "val_SS_loss": val_SS_loss,
+                "val_FS_loss": val_FS_loss,
+                "Learning Rate": self.trainer.optimizers[0].param_groups[0]['lr'],
+                "Epoch Time": self.epoch_time,
+                "train_plot": wandb.Image(self.out_0) if visuals else None,
+                "val_plot": wandb.Image(self.out_1) if visuals else None,
+                "val_forward_plot": wandb.Image(self.out_2) if visuals else None,
+                #"val_anim": wandb.Video(self.out_3, format="gif") if visuals else None,
+                "Log Time": self.log_time
+            })
+
         self.train_losses = []
         self.val_SS_losses = []
         self.val_FS_losses = []
+
+    def make_anim(self, output_path, device='cuda'):
+        model.eval()
+        with torch.no_grad():
+            dataset_idx = torch.randint(0, len(self.trainer.datamodule.val_datasets), (1,)).item()
+            traj_idx = self.trainer.datamodule.val_samplers[dataset_idx].random_val_traj()
+            val_traj = self.trainer.datamodule.val_datasets[dataset_idx].dataset.reader.get_single_traj(traj_idx)
+
+            front = val_traj[0].unsqueeze(0).to(device)
+            stacked_pred = rollout(front, self.model, len(val_traj))
+            stacked_pred, stacked_true = magnitude_vel(stacked_pred), magnitude_vel(val_traj)
+            dataset_name = self.trainer.datamodule.val_datasets[dataset_idx].dataset.reader.name
+            animate_rollout(stacked_pred, stacked_true, dataset_name, output_path)
+
+    def make_plot(self, output_path, mode='val', device='cuda'):
+        self.model.eval()
+
+        if mode == 'val':
+            loader = iter(self.trainer.datamodule.val_dataloader()[0])
+        elif mode == 'train':
+            loader = iter(self.trainer.datamodule.train_dataloader())
+        elif mode == 'val_forward':
+            loader = iter(self.trainer.datamodule.val_dataloader()[1])
+        else:
+            raise ValueError('PLOTMODE NOT RECOGNIZED')
+
+        front, label = next(loader)
+        front, label = front.to(device), label.to(device)
+        front, label = front[0].unsqueeze(0), label[0].unsqueeze(0)
+
+        with torch.no_grad():
+            pred = self(front)
+
+        front_x, front_y = front[0, 0].cpu(), front[0, 1].cpu()
+        pred_x, pred_y = pred[0, 0].cpu(), pred[0, 1].cpu()
+        label_x, label_y = label[0, 0].cpu(), label[0, 1].cpu()
+        diff_x, diff_y = (label_x - pred_x).abs(), (label_y - pred_y).abs()
+
+        fig, axes = plt.subplots(2, 4, figsize=(16, 8))
+        fig.suptitle(f"Epoch {self.trainer.current_epoch}")
+
+        titles = ["Front", "Pred", "True", "Diff"]
+        axes[0, 0].imshow(front_x, cmap='viridis')
+        axes[0, 1].imshow(pred_x, cmap='viridis')
+        axes[0, 2].imshow(label_x, cmap='viridis')
+        axes[0, 3].imshow(diff_x, cmap='magma')
+        axes[1, 0].imshow(front_y, cmap='viridis')
+        axes[1, 1].imshow(pred_y, cmap='viridis')
+        axes[1, 2].imshow(label_y, cmap='viridis')
+        axes[1, 3].imshow(diff_y, cmap='magma')
+
+        for i in range(4):
+            axes[0, i].set_title(titles[i])
+            axes[1, i].set_title(titles[i])
+        axes[0, 0].set_ylabel(r"$v_x$")
+        axes[1, 0].set_ylabel(r"$v_y$")
+        for ax in axes.flat:
+            ax.set_xticks([])
+            ax.set_yticks([])
+            for spine in ax.spines.values():
+                spine.set_visible(False)
+
+        plt.tight_layout()
+        plt.savefig(output_path)
+        plt.close()
+
 
 class MTTdata(pl.LightningDataModule):
     def __init__(self, cb, cd, cm, ct):
@@ -286,106 +378,3 @@ class MTTdata(pl.LightningDataModule):
             shuffle=True,
             pin_memory=self.ct.pin_memory
         )
-
-class LoggerCallback(pl.Callback):
-    def __init__(self, cb, cd, cm, ct):	
-        super().__init__()
-        self.cb = cb
-        self.cd = cd
-        self.cm = cm
-        self.ct = ct
-        self.out_0 = self.cb.save_path + self.cb.folder_out + self.ct.plottrain_out
-        self.out_1 = self.cb.save_path + self.cb.folder_out + self.ct.plotval_out
-        self.out_2 = self.cb.save_path + self.cb.folder_out + self.ct.plotvalf_out
-        self.out_3 = self.cb.save_path + self.cb.folder_out + self.ct.anim_out
-        self.callback_start_time = None
-
-    def on_validation_epoch_end(self, trainer, model):
-        if trainer.sanity_checking:
-            return
-        epoch = trainer.current_epoch
-        if self.cb.viz and epoch % self.cb.viz_freq == 0:
-            self.callback_start_time = time.time()
-            device = model.device
-
-            self.make_plot(model, self.out_1, mode='val', device=device)
-            self.make_plot(model, self.out_0, mode='train', device=device)
-            self.make_plot(model, self.out_2, mode='val_forward', device=device)
-            
-            #self.make_anim(model, self.out_3, device=device)
-
-            self.callback_start_time = time.time() - self.callback_start_time
-            trainer.logger.experiment.log({
-                "epoch": epoch,
-                "train_plot": wandb.Image(self.out_0),
-                "val_plot": wandb.Image(self.out_1),
-                "val_forward_plot": wandb.Image(self.out_2),
-                #"val_anim": wandb.Video(self.out_3, format="gif"),
-                "Callback Time": self.callback_start_time
-            })
-
-    def make_anim(self, model, output_path, device):
-        model.eval()
-        with torch.no_grad():
-            dataset_idx = torch.randint(0, len(model.trainer.datamodule.val_datasets), (1,)).item()
-            traj_idx = model.trainer.datamodule.val_samplers[dataset_idx].random_val_traj()
-            val_traj = model.trainer.datamodule.val_datasets[dataset_idx].dataset.reader.get_single_traj(traj_idx)
-
-            front = val_traj[0].unsqueeze(0).to(device)
-            stacked_pred = rollout(front, model, len(val_traj))
-            stacked_pred, stacked_true = magnitude_vel(stacked_pred), magnitude_vel(val_traj)
-            dataset_name = model.trainer.datamodule.val_datasets[dataset_idx].dataset.reader.name
-            animate_rollout(stacked_pred, stacked_true, dataset_name, output_path)
-
-    def make_plot(self, model, output_path, mode='val', device='cuda'):
-        model.eval()
-
-        if mode == 'val':
-            loader = iter(model.trainer.datamodule.val_dataloader()[0])
-        elif mode == 'train':
-            loader = iter(model.trainer.datamodule.train_dataloader())
-        elif mode == 'val_forward':
-            loader = iter(model.trainer.datamodule.val_dataloader()[1])
-        else:
-            raise ValueError('PLOTMODE NOT RECOGNIZED')
-
-        front, label = next(loader)
-        front, label = front.to(device), label.to(device)
-        front, label = front[0].unsqueeze(0), label[0].unsqueeze(0)
-
-        with torch.no_grad():
-            pred = model(front)
-
-        front_x, front_y = front[0, 0].cpu(), front[0, 1].cpu()
-        pred_x, pred_y = pred[0, 0].cpu(), pred[0, 1].cpu()
-        label_x, label_y = label[0, 0].cpu(), label[0, 1].cpu()
-        diff_x, diff_y = torch.zeros(front_x.shape), torch.zeros(front_y.shape)
-        #diff_x, diff_y = (label_x - pred_x).abs(), (label_y - pred_y).abs()
-
-        fig, axes = plt.subplots(2, 4, figsize=(16, 8))
-        fig.suptitle(f"Epoch {model.trainer.current_epoch}")
-
-        titles = ["Input", "Prediction", "Target", "Difference"]
-        axes[0, 0].imshow(front_x, cmap='viridis')
-        axes[0, 1].imshow(pred_x, cmap='viridis')
-        axes[0, 2].imshow(label_x, cmap='viridis')
-        axes[0, 3].imshow(diff_x, cmap='magma')
-        axes[1, 0].imshow(front_y, cmap='viridis')
-        axes[1, 1].imshow(pred_y, cmap='viridis')
-        axes[1, 2].imshow(label_y, cmap='viridis')
-        axes[1, 3].imshow(diff_y, cmap='magma')
-
-        for i in range(4):
-            axes[0, i].set_title(titles[i])
-            axes[1, i].set_title(titles[i])
-        axes[0, 0].set_ylabel(r"$v_x$")
-        axes[1, 0].set_ylabel(r"$v_y$")
-        for ax in axes.flat:
-            ax.set_xticks([])
-            ax.set_yticks([])
-            for spine in ax.spines.values():
-                spine.set_visible(False)
-
-        plt.tight_layout()
-        plt.savefig(output_path)
-        plt.close()
