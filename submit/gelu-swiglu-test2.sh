586381d7f0701ce10dd2e6cf0493b37277f6b582
Tom Harmsen <tharmsen@gcn3.local.snellius.surf.nl>
2025-05-11 15:17:48 +0200

    swiglu test

---
+++ submit/gelu-swiglu-test2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=mtt_h100
+#SBATCH --output=slurmlogs/%x_%j.out
+#SBATCH --error=slurmlogs/%x_%j.err
+#SBATCH --partition=gpu_mig
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-node=1
+
+module load 2024
+module spider CUDA/12.6.0
+module load Anaconda3/2024.06-1
+source "/sw/arch/RHEL9/EB_production/2024/software/Anaconda3/2024.06-1/etc/profile.d/conda.sh"
+conda activate grad312
+cd AutoregressiveNeuralOperators
+
+export CUDA_HOME=/sw/arch/RHEL9/EB_production/2024/software/CUDA/12.6.0
+
+python src/train.py --CB wandb_highfreq --CD quick3 --CM swiglu-test --out swiglu
