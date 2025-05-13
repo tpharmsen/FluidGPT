139113c5390beb2433a61c1541574879e87a65ae
tpharmsen <t.p.harmsen@student.tue.nl>
2025-05-13 02:11:01 +0200

    distributed sampler for single filereads

--- submit/fullh100node.sh
+++ submit/fullh100node.sh
@@ -3,13 +3,13 @@
 #SBATCH --output=slurmlogs/%x_%j.out
 #SBATCH --error=slurmlogs/%x_%j.err
 #SBATCH --partition=gpu_h100
-#SBATCH --time=08:00:00
+#SBATCH --time=06:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-node=4
 
 module load 2024
-module spider CUDA/12.6.0
+module load CUDA/12.6.0
 module load Anaconda3/2024.06-1
 source "/sw/arch/RHEL9/EB_production/2024/software/Anaconda3/2024.06-1/etc/profile.d/conda.sh"
 conda activate grad312
@@ -17,4 +17,13 @@ cd AutoregressiveNeuralOperators
 
 export CUDA_HOME=/sw/arch/RHEL9/EB_production/2024/software/CUDA/12.6.0
 
-python src/train.py --CB wandb_highfreq --CD 5set-fourier --CT deepspeed --CM swiglu --out fullh100node
+echo "Running on host: $(hostname)"
+echo "CUDA devices visible: $CUDA_VISIBLE_DEVICES"
+nvidia-smi
+
+torchrun --nproc_per_node=4 src/train.py \
+    --CB wandb_highfreq \
+    --CD 5set-fourier \
+    --CT deepspeed \
+    --CM swiglu \
+    --out fullh100node_2
