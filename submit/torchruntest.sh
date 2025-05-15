6c311dc32b75bb4e2619eac569a1916688e4b57a
Tom Harmsen <tharmsen@gcn4.local.snellius.surf.nl>
2025-05-15 15:45:34 +0200

    srun

--- submit/torchruntest.sh
+++ submit/torchruntest.sh
@@ -22,7 +22,7 @@ echo "Running on host: $(hostname)"
 echo "CUDA devices visible: $CUDA_VISIBLE_DEVICES"
 nvidia-smi
 
-torchrun --nproc_per_node=4 src/train.py \
+srun src/train.py \
     --CB wandb_highfreq \
     --CD 5set-fourier \
     --CT deepspeed \
