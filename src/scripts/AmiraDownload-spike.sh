db07a8fa08ce333c9ee1e25c460c084a622235f8
Harmsen <t.p.harmsen@student.tue.nl>
2025-05-26 14:23:22 +0200

    spike

---
+++ src/scripts/AmiraDownload-spike.sh
@@ -0,0 +1,24 @@
+#wget https://libdrive.ethz.ch/index.php/s/lv7dV40oYlkWJiC/download?path=%2F&files=0000.am 0000 t/m 7999
+#wget https://libdrive.ethz.ch/index.php/s/lv7dV40oYlkWJiC/download?path=%2F"&"files=0000.am
+
+cd datasets/prjs1359
+mkdir -p AmiraSet 
+
+n=500  # number of elements to select
+max=7999
+
+# Compute interval step
+step=$((max / (n - 1)))
+
+for ((i = 0; i < n; i++)); do
+    val=$((i * step))
+    fname=$(printf "%04d" $val)
+    filepath="AmiraSet/${fname}.am"
+
+    if [[ -f "$filepath" ]]; then
+        echo "File $filepath already exists. Skipping."
+    else
+        echo "Downloading ${fname}.am..."
+        wget -O "$filepath" "https://libdrive.ethz.ch/index.php/s/lv7dV40oYlkWJiC/download?path=%2F&files=${fname}.am"
+    fi
+done
