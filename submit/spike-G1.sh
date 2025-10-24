2eed6d28a89057ffe182e5f5a1d53dfd9c955b05
tpharmsen <t.p.harmsen@student.tue.nl>
2025-10-24 17:05:05 +0200

    spike container submit cmd

--- submit/spike-G1.sh
+++ submit/spike-G1.sh
@@ -5,6 +5,8 @@ source .env
 set +a
 
 MULTIPLIER=${1:-1}
+CONTAINER_NAME=${2:-fluidgpt-clitest}
+COMMAND_ARGS=${3:-"--trainer FM --CB spike-high --CD spike-gauss --CM fm-test-B --CT fm-b200-doublegauss5 --out ${CONTAINER_NAME}"}
 
 CPU_CORE_REQUEST=$((4 * MULTIPLIER))
 CPU_CORE_LIMIT=$((25 * MULTIPLIER))
@@ -14,7 +16,7 @@ GPU_DEVICES_REQUEST=$((1 * MULTIPLIER))
 
 echo "GPU devices request: $GPU_DEVICES_REQUEST"
 
-runai workspace submit fluidgpt-testsubmit-cli \
+runai workspace submit $CONTAINER_NAME \
 --image "harbor.spike.tue.nl/fluidgpt/fluidgpt-fm-ssh:latest" \
 --project "fluidgpt" \
 --cpu-core-limit $CPU_CORE_LIMIT \
@@ -27,4 +29,4 @@ runai workspace submit fluidgpt-testsubmit-cli \
 --existing-pvc "claimname=fluidgpt,path=/data" \
 --git-sync "name=fluidgpt,repository=https://github.com/tpharmsen/FluidGPT,path=/code/,rev=main" \
 --environment WANDB_API_KEY="$VAR3" \
---command -- torchrun --nproc-per-node=$GPU_DEVICES_REQUEST src/train.py --trainer FM --CB spike-high --CD spike-gauss --CM fm-test-B --CT fm-b200-doublegauss6 --out SpikeCLI-test
+--command -- torchrun --nproc-per-node=$GPU_DEVICES_REQUEST src/train.py $COMMAND_ARGS
