4c90e1af39253e34a234f029d8b2586c0bae9075
tpharmsen <t.p.harmsen@student.tue.nl>
2025-10-26 02:52:16 +0200

    runs 26oct

---
+++ submit/run26oct.sh
@@ -0,0 +1,9 @@
+runai login
+submit/spike-G1.sh 1 fmtest-a-c1 "--trainer FM --CB spike-high --CD spike-gauss --CM fm-test-A-c1 --CT fm-b200-doublegauss6 --out fmtest-a-c1" 
+submit/spike-G1.sh 1 fmtest-a-h1 "--trainer FM --CB spike-high --CD spike-gauss --CM fm-test-A-h1 --CT fm-b200-doublegauss6 --out fmtest-a-h1"
+submit/spike-G1.sh 1 fmtest-a-h2 "--trainer FM --CB spike-high --CD spike-gauss --CM fm-test-A-h2 --CT fm-b200-doublegauss6 --out fmtest-a-h2"
+submit/spike-G1.sh 1 fmtest-a-h3 "--trainer FM --CB spike-high --CD spike-gauss --CM fm-test-A-h3 --CT fm-b200-doublegauss6 --out fmtest-a-h3"
+submit/spike-G1.sh 1 fmtest-a-c2 "--trainer FM --CB spike-high --CD spike-gauss --CM fm-test-A-c2 --CT fm-b200-doublegauss6 --out fmtest-a-c2" 
+submit/spike-G1.sh 1 fmtest-b-e2 "--trainer FM --CB spike-high --CD spike-gauss --CM fm-test-B-e2 --CT fm-b200-doublegauss6 --out fmtest-b-e2"
+submit/spike-G1.sh 1 fmtest-b-p2 "--trainer FM --CB spike-high --CD spike-gauss --CM fm-test-B-p2 --CT fm-b200-doublegauss6 --out fmtest-b-p2"
+submit/spike-G1.sh 1 fmtest-b-sd2 "--trainer FM --CB spike-high --CD spike-gauss --CM fm-test-B-sd2 --CT fm-b200-doublegauss6 --out fmtest-b-sd2"
