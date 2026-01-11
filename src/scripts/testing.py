897df97c7f4875c664dc0d614fee3bd8e7ba7bf4
tpharmsen <t.p.harmsen@student.tue.nl>
2026-01-11 16:07:30 +0100

    wip

--- src/scripts/testing.py
+++ src/scripts/testing.py
@@ -1,9 +1,35 @@
 # import libraries
-import torch
-import matplotlib.pyplot as plt
-import argparse
-import os
+import torch 
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, ConcatDataset, random_split, Subset
+from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau, CosineAnnealingLR, SequentialLR, ConstantLR
+from datetime import datetime
+import torch.distributed as dist
+from torch.utils.data.distributed import DistributedSampler
+import time
+import wandb
 import yaml
+import random
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import numpy as np
+import matplotlib.animation as animation
+import os
+import subprocess
+import platform
+import json
+import argparse
+
+from dataloaders import *
+from dataloaders import PREPROC_MAPPER
+from dataloaders.utils import get_dataset, ZeroShotSamplerReduced, spatial_resample
+#from trainers.utils import make_plot, animate_rollout, magnitude_vel, rollout
+from trainers.utils import animate_rollout, magnitude_vel, rollout_det, compute_energy_enstrophy_spectra
+from modelComp.utils import ACT_MAPPER, SKIPBLOCK_MAPPER
 
 plt.style.use('dark_background')
 plt.rcParams['figure.facecolor'] = '#1F1F1F'
@@ -12,7 +38,6 @@ plt.rcParams['savefig.facecolor'] = '#1F1F1F'
 torch.set_float32_matmul_precision('medium')
 
 # read in all config files
-
 
 class DotDict(dict):
     def __init__(self, mapping=None):
@@ -35,49 +60,184 @@ def load_yaml_as_dotdict(filepath):
         data = yaml.safe_load(file) or {}  # for if yaml empty lined
     return DotDict(data)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--CB", type=str, default="std")
-parser.add_argument("--CD", type=str, default="std")
-parser.add_argument("--CM", type=str, default="std")
-parser.add_argument("--CT", type=str, default="std")
-parser.add_argument("--trainer", type=str, default="MTT")
-parser.add_argument("--out", type=str, default=None)
-args = parser.parse_args()
+def read_command():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--CB", type=str, default="std")
+    parser.add_argument("--CD", type=str, default="std")
+    parser.add_argument("--CM", type=str, default="std")
+    parser.add_argument("--CT", type=str, default="std")
+    parser.add_argument("--trainer", type=str, default="MTT")
+    parser.add_argument("--out", type=str, default=None)
+    args = parser.parse_args()
 
-if os.path.exists("conf/base/" + args.CB + ".yaml"):
-    cb = load_yaml_as_dotdict("conf/base/" + args.CB + ".yaml")
-else:
-    raise FileNotFoundError(f"Config file {args.CB}.yaml not found.")
-if os.path.exists("conf/data/" + args.CD + ".yaml"):
-    cd = load_yaml_as_dotdict("conf/data/" + args.CD + ".yaml")
-else:
-    raise FileNotFoundError(f"Config file {args.CD}.yaml not found.")
-if os.path.exists("conf/model/" + args.CM + ".yaml"):
-    cm = load_yaml_as_dotdict("conf/model/" + args.CM + ".yaml")
-else:
-    raise FileNotFoundError(f"Config file {args.CM}.yaml not found.")
-if os.path.exists("conf/training/" + args.CT + ".yaml"):
-    ct = load_yaml_as_dotdict("conf/training/" + args.CT + ".yaml")
-else:
-    raise FileNotFoundError(f"Config file {args.CT}.yaml not found.")
+    if os.path.exists("conf/base/" + args.CB + ".yaml"):
+        cb = load_yaml_as_dotdict("conf/base/" + args.CB + ".yaml")
+    else:
+        raise FileNotFoundError(f"Config file {args.CB}.yaml not found.")
+    if os.path.exists("conf/data/" + args.CD + ".yaml"):
+        cd = load_yaml_as_dotdict("conf/data/" + args.CD + ".yaml")
+    else:
+        raise FileNotFoundError(f"Config file {args.CD}.yaml not found.")
+    if os.path.exists("conf/model/" + args.CM + ".yaml"):
+        cm = load_yaml_as_dotdict("conf/model/" + args.CM + ".yaml")
+    else:
+        raise FileNotFoundError(f"Config file {args.CM}.yaml not found.")
+    if os.path.exists("conf/training/" + args.CT + ".yaml"):
+        ct = load_yaml_as_dotdict("conf/training/" + args.CT + ".yaml")
+    else:
+        raise FileNotFoundError(f"Config file {args.CT}.yaml not found.")
+    return cb, cd, cm, ct, args.trainer
+
 print("Configs loaded.")
-raise NotImplementedError("Testing script not yet implemented.")
+#raise NotImplementedError("Testing script not yet implemented.")
+
 # import data
+
 
 # load model
 
+class ModelValidation:
+    def __init__(self, cb, cd, cm, ct, trainer, model_path):
+        self.cb = cb
+        self.cd = cd
+        self.cm = cm
+        self.ct = ct
+        self.trainer = trainer
+        self.model_path = model_path
 
-def calculate_ss_error_per_dataset():
-    # absolute and relative error per dataset?
-    pass
+        self.init_modules()
 
-def calculate_rollout_error_per_dataset():
-    # not sure yet
-    pass
+    def init_modules(self):
+        # load model
+        self.load_model()
+        # load dataloaders
+        self.load_dataloaders()
 
-def calculate_spectra_plots_per_dataset():
-    # not sure yet
-    pass
+    def load_model(self):
+        if self.cm.model_name == "FluidGPT":
+            from modelComp.FluidGPT_B import FluidGPT_B
+            self.model = FluidGPT_B(emb_dim=self.cm.emb_dim,
+                            data_dim=[self.ct.batch_size, self.cm.temporal_bundling, self.cm.in_channels, self.cd.resample_shape, self.cd.resample_shape],
+                            patch_size=(self.cm.patch_size, self.cm.patch_size),
+                            hiddenout_dim=self.cm.hiddenout_dim,
+                            depth=self.cm.depth,
+                            stage_depths=self.cm.stage_depths,
+                            num_heads=self.cm.num_heads,
+                            window_size=self.cm.window_size,
+                            use_flex_attn=self.cm.use_flex_attn,
+                            act=ACT_MAPPER[self.cm.act],
+                            skip_connect=SKIPBLOCK_MAPPER[self.cm.skipblock],
+                            gradient_flowthrough=self.cm.gradient_flowthrough,
+                            ).cuda()
+        else:
+            raise ValueError('MODEL NOT RECOGNIZED')   
+        self.model.load_state_dict(torch.load(self.model_path))
+        self.model.eval()
+        print("Model loaded from", self.model_path)
+
+    def load_dataloaders(self):
+        
+        # assume preprocessing has already been done !
+        
+        self.val_datasets = []
+        self.val_samplers = []
+        self.val_loaders = []
+
+        means, stds, sizes = [], [], []
+                
+        for item in self.cd.datasets:
+            preproc_savepath = str(self.cb.data_base + 'preproc_' + item["name"])
+            dataset_SS = DiskDatasetDiv(preproc_savepath, temporal_bundling=self.cm.temporal_bundling, forward_steps=1)
+            #dataset_FS = DiskDatasetDiv(preproc_savepath, temporal_bundling=self.cm.temporal_bundling, forward_steps=self.ct.forward_steps_loss)
+
+            # generate random seed
+            seed = self.cd.seed
+            #random_seed = random.randint(0, 10000)
+            #train_sampler = ZeroShotSamplerReduced(dataset_SS, train_ratio=self.ct.train_ratio, 
+            #                                       split="train", seed=seed, skip_timesteps=item["timesample"])
+            val_sampler = ZeroShotSamplerReduced(dataset_SS, train_ratio=self.ct.train_ratio, 
+                                                 split="val", seed=seed, skip_timesteps=item["timesample"])
+            #val_forward_sampler = ZeroShotSamplerReduced(dataset_FS, train_ratio=self.ct.train_ratio, split="val", seed=seed, forward_steps=self.ct.forward_steps_loss)
+
+            #self.train_datasets.append(Subset(dataset_SS, train_sampler.indices))
+            self.val_datasets.append(Subset(dataset_SS, val_sampler.indices))
+            #self.val_forward_datasets.append(Subset(dataset_FS, val_forward_sampler.indices))
+            #self.train_samplers.append(train_sampler)
+            self.val_samplers.append(val_sampler)
+            #self.val_forward_samplers.append(val_forward_sampler)
+            
+            #torch.synchronize() 
+            split = {
+                "name": item["name"],
+                "seed": seed,
+                #"train_trajs": train_sampler.train_trajs,
+                "val_trajs": val_sampler.val_trajs,
+                #"val_forward_trajs": val_forward_sampler.val_trajs,
+                #"train_idxs": train_sampler.indices,
+                "val_idxs": val_sampler.indices,
+                #"val_forward_idxs": val_forward_sampler.indices,
+            }
+            if self.cb.save_on:
+                save_split_path = None
+                if save_split_path is None:
+                    raise ValueError("ModelCheckpoint callback not found, unable to save trajectory split.")
+                with open(save_split_path, "w") as f:
+                    json.dump(split, f, indent=0)
+                if wandb.run is not None:
+                    wandb.save(save_split_path)
+            
+            mean_i = dataset_SS.avg
+            std_i = dataset_SS.std
+            N_i = np.prod(dataset_SS.datashape)
+
+            means.append(mean_i)
+            stds.append(std_i)
+            sizes.append(N_i)
+            print("dataset", item["name"], "loaded,", np.prod(dataset_SS.datashape), "elements")
+
+        means = np.array(means)
+        stds = np.array(stds)
+        sizes = np.array(sizes)
+
+        self.global_mean = np.sum(sizes * means) / np.sum(sizes)
+        self.global_std = np.sqrt(np.sum(sizes * (stds**2 + (means - self.global_mean)**2)) / np.sum(sizes))
+        if self.ct.normalize:
+            for dataset_list in [self.val_datasets]: #, self.val_forward_datasets]:
+                for subset in dataset_list:
+                    subset.dataset.avgnorm = self.global_mean
+                    subset.dataset.stdnorm = self.global_std
+                
+        #self.train_dataset = ConcatDataset(self.train_datasets)
+        #self.val_dataset = ConcatDataset(self.val_datasets)
+        #self.val_forward_dataset = ConcatDataset(self.val_forward_datasets)
+        print("datasets ready, now creating dataloaders...")
+        dataloader = DataLoader(self.val_dataset,
+            batch_size=self.ct.batch_size,
+            shuffle=False,
+            pin_memory=self.ct.pin_memory, 
+            num_workers=self.ct.num_workers,
+            persistent_workers=self.ct.persistent_workers if self.ct.num_workers > 0 else False,
+            prefetch_factor=self.ct.prefetch_factor if self.ct.num_workers > 0 else None
+        )
+        self.val_loaders.append(dataloader)
+        print("dataloaders created.")
+        print(len(self.val_loaders))
+               
+
+    def calculate_ss_error_per_dataset():
+        # absolute and relative error per dataset?
+        for i, dataset in enumerate(self.val_datasets):
+        pass
+
+    def calculate_rollout_error_per_dataset():
+        # not sure yet
+        pass
+
+    def calculate_spectra_plots_per_dataset():
+        # not sure yet
+        pass
 
 if __name__ == "__main__":
-    calculate_ss_error_per_dataset()
+    cb, cd, cm, ct, trainer = read_command()
+    model_validation = ModelValidation(cb, cd, cm, ct, trainer, model_path)
+    model_validation.calculate_ss_error_per_dataset()
