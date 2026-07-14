008401cfedda991ff10853868177c24bf718469a
tpharmsen <t.p.harmsen@student.tue.nl>
2026-07-14 17:38:01 +0200

    svg figs

---
+++ report/figs/latex_to_svg.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Usage:
+# ./latex_to_svg.sh input.tex output.svg
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 input.tex output.svg"
+    exit 1
+fi
+
+TEX_FILE="$1"
+SVG_FILE="$2"
+
+# Get filename without extension
+BASE_NAME="${TEX_FILE%.tex}"
+
+# Compile LaTeX to DVI
+latex -latex "$TEX_FILE"
+
+if [ $? -ne 0 ]; then
+    echo "LaTeX compilation failed."
+    exit 1
+fi
+
+# Convert DVI to SVG
+dvisvgm --font-format=woff "${BASE_NAME}.dvi" -o "$SVG_FILE"
+
+if [ $? -ne 0 ]; then
+    echo "SVG conversion failed."
+    exit 1
+fi
+
+# Replace colors
+sed -i \
+    -e 's/#000000/#ffffff/gI' \
+    -e 's/#1c1c1c/#ffffff/gI' \
+    -e 's/#000/#ffffff/gI' \
+    "$SVG_FILE"
+
+echo "Created: $SVG_FILE"
---
+++ slide-deck/start-deck.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+npx serve . --listen tcp://127.0.0.1:3000
